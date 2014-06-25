3ccb2433eccc23dda601bf44ec6d4f3ddd8d5306
Swaff <swaff68@gmail.com>
2014-06-25 11:02:01 -0600

    finished

--- main.js
+++ main.js
@@ -1,3 +1,13 @@
-$(document).on('ready', function() {
-  
+//create a variable, target the variable against a class or id
+var welcome = $('#welcome-message');
+//call the var and add .css; first is property to change, then the value 
+welcome.css('font-size', '72px');
+
+welcome.css({
+	'font-size': '72px',
+	'color': 'red',
+	'font-weight': 'bold'
 });
+
+$('.start').css ('color', 'blue')
+
